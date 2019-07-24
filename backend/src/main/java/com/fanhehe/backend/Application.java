30af05608e2052e3c8a62d02ae8c773ba816a98f
franfan <13889441200@163.com>
2019-07-24 16:22:27 +0800

    backend 支持发布环境

--- backend/src/main/java/com/fanhehe/backend/Application.java
+++ backend/src/main/java/com/fanhehe/backend/Application.java
@@ -2,8 +2,16 @@ package com.fanhehe.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class
+    })
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
