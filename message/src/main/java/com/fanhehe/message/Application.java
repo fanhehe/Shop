01e02a8fb5258eaedf86eda9793ed4e287a17c22
franfan <13889441200@163.com>
2019-07-24 12:20:20 +0800

    继承 SpringBootServletInitializer

--- message/src/main/java/com/fanhehe/message/Application.java
+++ message/src/main/java/com/fanhehe/message/Application.java
@@ -1,13 +1,21 @@
 package com.fanhehe.message;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //@MapperScan(basePackages= {"com.fanhehe.message.dao"})
-public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+public class Application extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
